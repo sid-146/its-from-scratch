d8665380a6010779632d0e6019eca6331c0b4f8a
sid-146 <sudhanwa.kaveeshwar@gmail.com>
2026-02-22 23:28:27 +0530

    Updated linear regression folder

--- supervised_learning/linear_regression/data_gen.py
+++ supervised_learning/linear_regression/data_gen.py
@@ -1,1 +1,11 @@
 from sklearn.datasets import make_regression
+
+
+def get_data(n_samples=1000, n_feature=5, noise=15):
+    data = make_regression(
+        n_samples=n_samples,
+        n_features=n_feature,
+        noise=noise,
+        random_state=42,
+    )
+    return data
